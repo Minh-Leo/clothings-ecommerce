f1f800caaf39bd1b03f5f2dcc68d975028892d2e
Minh Bui <tranduyminh.bui@gmail.com>
2019-12-29 00:11:24 +1030

    updated addToCart function, cart utils to group items in cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(i => (
+        <h1>{i.name}</h1>
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+export default connect(mapStateToProps, null)(CartDropdown);
